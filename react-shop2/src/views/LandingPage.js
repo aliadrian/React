9f14b2dd5bc593d67209a61c709b919484f4b38d
Ali Adrian <alinasrat95@gmail.com>
2020-09-03 20:36:00 +0200

    first commit

---
+++ react-shop2/src/views/LandingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  const style = {
+    marginTop: "10rem",
+  };
+
+  return (
+    <div style={style}>
+      <h1 className="text-center">Welcome</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ react-shop2/src/views/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductCardDeck from '../components/products/ProductCardDeck';
+
+
+const Products = () => {
+  return (
+    <div>
+      <ProductCardDeck />
+    </div>
+  )
+}
+
+export default Products;
